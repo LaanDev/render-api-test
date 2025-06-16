014545ee8bafa9e2e48ffd3fd0d03fe3b197d9c1
LaanDev <laancarlos@outlook.com>
2025-06-16 11:17:26 -0300

    segundo cmmit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'OK' });
+});
+
+app.listen(port, () => {
+  console.log(`API rodando na porta ${port}`);
+});
